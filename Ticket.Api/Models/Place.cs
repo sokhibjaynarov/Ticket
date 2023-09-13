daddf859c5b2a186d9b45ad5190440898365c51d
Azizbek Askarov <azizasqarov773@gmail.com>
2023-09-13 10:40:17 +0500

    Data Add Place Model

---
+++ Ticket.Api/Models/Place.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket.Api.Models;
+
+public class Place
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public long Capacity { get; set; }
+    public long Rows { get; set; }
+    public long SeatsInRow { get; set; }
+
+}
